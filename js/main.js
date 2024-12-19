f3d84220d61424bbe7288c5557c9cc200c04e893
Allan Silva <allandasilva33@outlook.com>
2024-12-19 03:08:10 -0300

    Alteração de todo o estilo da página + Criação dos efeitos de scroll

--- js/main.js
+++ js/main.js
@@ -31,3 +31,21 @@ setInterval(()=>{
         el.classList.toggle('skills-text-animation');
     })
 }, 3000);
+
+// Scroll Animation
+const ELE_EFFECT = [...document.querySelectorAll('.hidden-effect-01, .hidden-effect-02, .hidden-effect-03, .hidden-effect-04, .hidden-effect-05')];
+
+const MY_OBSERVER = new IntersectionObserver((pEle)=>{
+    pEle.map((ele)=>{
+        if(ele.isIntersecting){
+            ele.target.classList.add('visible-effect');
+        }else{
+            ele.target.classList.remove('visible-effect');
+        }
+    })
+});
+
+ELE_EFFECT.map((ele)=>{
+    MY_OBSERVER.observe(ele);
+});
+
